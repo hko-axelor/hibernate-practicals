05cca5af79e7fb82a7402c1fd46c753a1b3ac9cd
hko-axelor <hemang.axelor@gmail.com>
2021-09-02 11:14:19 +0530

    Hibernate Caching levels

---
+++ caching/lib/src/main/java/caching/Student.java
@@ -0,0 +1,35 @@
+package caching;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Cacheable
+@Cache(usage=CacheConcurrencyStrategy.READ_ONLY)
+public class Student {
+	@Id
+	private int st_id;
+	private String st_name;
+	public int getSt_id() {
+		return st_id;
+	}
+	public void setSt_id(int st_id) {
+		this.st_id = st_id;
+	}
+	public String getSt_name() {
+		return st_name;
+	}
+	public void setSt_name(String st_name) {
+		this.st_name = st_name;
+	}
+	@Override
+	public String toString() {
+		return "Student [st_id=" + st_id + ", st_name=" + st_name + "]";
+	}
+	
+	
+}
